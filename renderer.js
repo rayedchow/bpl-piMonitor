854b45bd1553c5d18bb7524e4b653ba1e1c4a698
voomp <bat7gamer7@gmail.com>
2021-08-27 15:40:46 -0400

    renderer.js fix

--- renderer.js
+++ renderer.js
@@ -3,19 +3,35 @@
 // All of the Node.js APIs are available in this process.
 
 const serialport = require('serialport');
-const port = new serialport('/dev/cu.usbmodem14201', { baudRate: 9600 });
 
-port.on('open', () => {
-	console.log('pog the communication gateway between pi->arduino is now open.');
-});
+const portParser = () => {
+        serialport.list().then(portList => {
+                portList.forEach(dataPort => {
+                        if(dataPort.manufacturer == "Arduino (www.arduino.cc)") {
+				startListener(dataPort.path);
+                        	return;
+			}
+                });
+        });
+}
 
-port.on('data', data => {
-	console.log('pog new data from the other side of the coms gateway: ' + data);
-});
+portParser();
 
-const servo = () => {
-	console.log('hi');
-	port.write('servo-run');
-};
+const startListener = (portStr) => {
+	const port = new serialport(portStr, { baudRate: 9600 });
 
-document.getElementById('controller').onclick = servo;
+	port.on('open', () => {
+		console.log('pog the communication gateway between pi->arduino is now open.');
+	});
+
+	port.on('data', data => {
+		console.log('pog new data from the other side of the coms gateway: ' + data);
+	});
+
+	const servo = () => {
+        	console.log('hi');
+        	port.write('servo-run');
+	};
+
+	document.getElementById('controller').onclick = servo;
+}
--- servoPi/servoPi.ino
+++ servoPi/servoPi.ino
@@ -1,23 +1,60 @@
 #include <Servo.h>
 
+// Leg Servo Data
 Servo leg;
+int legPin = 9;
 int legPos = 0;
 
+// Ball Servo Data
+Servo ball;
+int ballPin = 8;
+int ballPos = 0;
+
+// Motion Sensor Data
+int sensorPin = 3;
+int val = 0;
+int calibrationSequence = 60;
+
 void setup() {
-  leg.attach(9);
+  pinMode(sensorPin, INPUT);
+  leg.attach(legPin);
+  ball.attach(ballPin);
+  
+  // Setting up SerialPort for Arduino-><-Pi communication.
   Serial.begin(9600);
   Serial.setTimeout(10);
+
+  // Calibration Sequence for motion sensor.
+  for (int i = 0; i < calibrationSequence; i++) {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  delay(50);
 }
 
 void loop() {
+  val = digitalRead(sensorPin);
+
+  // If serial port has available messages, read them and execute them.
   if(Serial.available() > 0) {
     String piRead = Serial.readString();
     String servRun = "servo-run";
     if (piRead == servRun) {
-      legPos += 30;
-      if(legPos > 180) legPos -= 180;
+      legPos += 90;
+      ballPos += 90;
+      if(legPos >= 180) legPos = 0;
+      if(ballPos >= 180) ballPos = 0;
     }
   }
+
+  // If the sensor is triggered, send message to Pi.
+  if(val == HIGH) {
+    Serial.print("sensor-execute");
+  }
+
+  // Update servo positions
   leg.write(legPos);
+  ball.write(ballPos);
   delay(100);
 }
